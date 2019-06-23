3ee11e206e3c10e909a8565b5f2b8d52c847a9ca
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-06-23 18:20:35 +0300

    Add Program

---
+++ src/Fonts/Program.elm
@@ -0,0 +1,32 @@
+module Fonts.Program exposing (..)
+
+import Browser exposing (..)
+import Fonts.Fonts as Fonts exposing (Fonts)
+import Fonts.Html as Html
+
+
+documentWithFonts :
+  {
+    init : flags -> ( model, Cmd msg ),
+    view : model -> Document msg,
+    fonts : Fonts,
+    update : msg -> model -> ( model, Cmd msg ),
+    subscriptions : model -> Sub msg
+  } ->
+  Program flags model msg
+documentWithFonts spec =
+  let
+    view model =
+      let
+        document = spec.view model
+        title = document.title
+        body = Html.style spec.fonts :: document.body
+        in { title = title, body = body }
+    in
+      document
+        {
+          init = spec.init,
+          view = view,
+          update = spec.update,
+          subscriptions = spec.subscriptions
+        }
